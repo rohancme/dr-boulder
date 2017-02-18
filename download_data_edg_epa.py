31c97282f75e74a7e9578ac3c99d8ca5fa4941f8
Rohan Chakravarthy <rohanc219@gmail.com>
2017-02-17 19:35:21 -0800

    cleanup

---
+++ download_data_edg_epa.py
@@ -0,0 +1,133 @@
+import os
+import requests
+from clint.textui import progress
+import argparse
+
+valid_formats = ('.xml', '.pdf', '.json', '.zip', '.csv', '.png', '.geojson',
+                 '.xlsx', '.kmz')
+
+binary_formats = ('.zip', '.png', '.kmz', '.xlsx', '.pdf')
+
+
+current_size = 0
+total_size = 0
+
+skip_list = []
+
+
+def handle_download(url, local_path, filename, max_size):
+    print "Attempting download of:" + url
+    global skip_list
+    try:
+        req_stream = requests.get(url, stream=True, verify=False)
+        req_stream.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print "Exception while trying to download: " + str(url)
+        print "Exception: " + str(e)
+        skip_list = skip_list + [url]
+        print "Added to skip list"
+        return
+
+    # just a catch all to make sure the script doesn't exit for a case
+    # I haven't thought of. That would be annoying with so many files!
+    try:
+        full_path = os.path.join(local_path, filename)
+        open_mode = 'wb' if filename.endswith(binary_formats) else 'w'
+        total_length = int(req_stream.headers.get('content-length'))
+        print "About to download file to: " + str(full_path)
+        print "FileSize(bytes): " + str(total_length)
+        with open(full_path, open_mode) as f:
+            if (current_size >= max_size):
+                print "Downloading the next file will hit storage limit. Stopping"
+                return
+            for chunk in progress.bar(req_stream.iter_content(chunk_size=1024),
+                                      expected_size=(total_length / 1024) + 1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+        return total_length
+    except:
+        print "Unexpected error trying to download url:" + str(url)
+        print "Adding it to the skip list"
+        skip_list = skip_list + [url]
+
+
+def get_data_list(filename):
+    # get data into list
+    with open(filename) as data_file:
+        data_text = data_file.readlines()
+    data_list = [x.strip() for x in data_text]
+    return data_list
+
+
+def download_all_files(data_links_filename,
+                       downloads_path,
+                       max_size,
+                       max_downloaded_files=5):
+    # get all the download links
+    data_list = get_data_list(data_links_filename)
+    counter = 0
+    current_size = 0
+
+    for data_path in data_list:
+        full_url_no_options = data_path.rsplit('?')[0]
+        url_without_domain = full_url_no_options.replace("http://", "")
+        url_without_domain = url_without_domain.replace("https://", "")
+        url_without_domain = url_without_domain.split('/', 1)[1]
+        path_and_filename = url_without_domain.rsplit('/', 1)
+        data_dir = path_and_filename[0]
+        filename = path_and_filename[1]
+        full_data_dir = os.path.join(downloads_path, *data_dir.split('/'))
+        if (os.path.exists(full_data_dir)):
+            print full_data_dir + "already exists"
+        else:
+            print "Creating dir: " + full_data_dir
+            os.makedirs(full_data_dir)
+            print full_data_dir + " created"
+        if (os.path.isfile(os.path.join(full_data_dir, filename))):
+            print "File already exists. Skipping.."
+            continue
+
+        current_size = current_size + handle_download(data_path, full_data_dir,
+                                                      filename, max_size)
+        if (current_size >= max_size):
+            break
+        counter = counter + 1
+        if (counter == max_downloaded_files):
+            break
+
+
+def create_downloads_folder(folder_name="downloads"):
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    downloads_path = os.path.join(base_path, folder_name)
+    if (os.path.exists(downloads_path)):
+        print downloads_path + " already exists..not creating folder"
+    else:
+        print "Creating folder: " + downloads_path
+        os.makedirs(downloads_path)
+    print "All downloaded data will be stored in:" + downloads_path
+
+if __name__ == '__main__':
+    default_max_size = 5000000000
+    max_in_gb = default_max_size / 1000000000
+    parser = argparse.ArgumentParser(description='Download data listed in a file')
+    parser.add_argument('--filename', metavar='F', type=str,
+                        required=True,
+                        help='The name of the file containing links' +
+                             '(one per line).Only direct download links!!')
+    parser.add_argument('--max_space', metavar='M', type=int,
+                        default=default_max_size,
+                        help='The maximum space you have available. Default: ' +
+                        str(max_in_gb) + 'GB')
+    parser.add_argument('--downloads_folder', metavar='D', type=str,
+                        default='downloads',
+                        help='The name of the downloads folder')
+
+    args = parser.parse_args()
+
+    create_downloads_folder(folder_name=args.downloads_folder)
+    download_all_files(data_links_filename=args.filename,
+                       downloads_path=args.downloads_folder,
+                       max_size=args.max_space,
+                       max_downloaded_files=5)
+    # download_all_files(data_links_filename="edg_epa_file_list.txt")
--- edg_epa_data_public/find_data_edg_epa.py
+++ edg_epa_data_public/find_data_edg_epa.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import re
-from clint.textui import progress
 import sys
 
 ip = 'http://134.67.99.116'
@@ -17,6 +16,7 @@ ftp_list = []
 skip_list = []
 crawl_list = []
 crawled = []
+output_data_list = []
 
 counter = 0
 
@@ -59,42 +59,15 @@ while (len(crawl_list) != 0):
         print "Length of file list:" + str(len(data_list)) + "\n"
     get_all_web_links(soup)
 
-base_path = os.path.dirname(os.path.realpath(__file__))
-downloads_path = os.path.join(base_path, 'downloads')
-if (os.path.exists(downloads_path)):
-    print downloads_path + " already exists..not creating folder"
-else:
-    print "Creating folder: " + downloads_path
-    os.makedirs(downloads_path)
-print "All downloaded data will be stored in:" + downloads_path
-
 counter = 0
 
-
-def handle_download(url, local_path, filename):
-    print "Attempting download of:" + url
-    try:
-        req_stream = requests.get(url, stream=True, verify=False)
-        req_stream.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print "Exception while trying to download: " + url
-        print "Exception: " + str(e)
-        global skip_list
-        skip_list = skip_list + [url]
-        print "Added to skip list"
-        return
-    full_path = os.path.join(local_path, filename)
-    open_mode = 'wb' if filename.endswith(binary_formats) else 'w'
-    with open(full_path, open_mode) as f:
-        total_length = int(req_stream.headers.get('content-length'))
-        for chunk in progress.bar(req_stream.iter_content(chunk_size=1024),
-                                  expected_size=(total_length / 1024) + 1):
-            if chunk:
-                f.write(chunk)
-                f.flush()
+max_size = 2000000000
+current_size = 0
+total_size = 0
 
 # default to a max of 5 downloaded files
 max_downloaded_files = int(sys.argv[1]) if (len(sys.argv) > 1) else 5
+
 
 for data_path in data_list:
     if (data_path.startswith('ftp:')):
@@ -108,18 +81,16 @@ for data_path in data_list:
         if (data_path not in skip_list):
             skip_list = skip_list + [data_path]
         continue
-    url_and_filename = data_path.rsplit('/', 1)
-    data_dir = url_and_filename[0]
-    filename = url_and_filename[1]
-    full_data_dir = os.path.join(downloads_path, *data_dir.split('/'))
-    if (os.path.exists(full_data_dir)):
-        print full_data_dir + "already exists"
-    else:
-        print "Creating dir: " + full_data_dir
-        os.makedirs(full_data_dir)
-        print full_data_dir + " created"
+    output_data_list = output_data_list + [ip + data_path]
 
-    handle_download(ip + data_path, full_data_dir, filename)
-    counter = counter + 1
-    if (counter == max_downloaded_files):
-        break
+with open("edg_epa_file_list.txt", "w") as data_file:
+    for data_path in output_data_list:
+        data_file.write("%s\n" % data_path)
+
+with open("edg_epa_ftp_file_list.txt", "w") as ftp_file:
+    for data_path in ftp_list:
+        ftp_file.write("%s\n" % data_path)
+
+with open("edg_epa_skipped_file_list.txt", "w") as skip_file:
+    for data_path in skip_list:
+        skip_file.write("%s\n" % data_path)
